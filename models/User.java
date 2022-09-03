f3f947107328b281b2e10527b2e6bab44280af5e
Robert-Martins <robertcantares@gmail.com>
2022-09-03 11:22:37 -0300

    feat/user-model-enhancement

--- models/User.java
+++ models/User.java
@@ -1,16 +1,34 @@
 package models;
 
+import java.util.Date;
 import java.util.List;
 
 public class User {
 
+    private String name;
+
     private List<Workspace> workspaces;
 
-    public User(List<Workspace> workspaces) {
+    private Date updateAt;
+
+    private Date createdAt;
+
+    public User(String name, List<Workspace> workspaces) {
+        this.setName(name);
         this.setWorkspaces(workspaces);
+        this.setUpdateAt(new Date());
+        this.setCreatedAt(new Date());
     }
 
     public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public List<Workspace> getWorkspaces() {
         return workspaces;
@@ -18,5 +36,21 @@ public class User {
 
     public void setWorkspaces(List<Workspace> workspaces) {
         this.workspaces = workspaces;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 }
